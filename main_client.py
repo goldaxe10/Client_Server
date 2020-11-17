4bda5e3d9d79a80d9f3f7a75c08923a37e0de6f7
goldaxe10 <zorg05mail@gmail.com>
2020-11-17 19:30:40 +0500

    update

--- main_client.py
+++ main_client.py
@@ -1,9 +1,16 @@
 import socket
 
 
+#while True:
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.connect(('MASTER-PC01', 5000))
 while True:
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.connect(('DESKTOP-24EDK13', 5000))
     x = input().encode('utf-8')
-    server_socket.send(x)
-    server_socket.close()
+    if x.decode('utf-8') == 'quit()':
+        server_socket.send(x)
+        server_socket.close()
+        break
+    else:
+        server_socket.send(x)
+
+    #server_socket.close()
